17302d97cd54fa86844e5e2cb52b7314e0cc6f6d
HONG GIBEOM <steaveall1@naver.com>
2017-12-09 19:50:34 +0900

    hihi

--- lock/while.c
+++ lock/while.c
@@ -1,14 +1,21 @@
+
 #include <stdio.h>
 #include <time.h>
-
+#include <pthread.h>
 
 int main(){
 
+struct timespec tstart={0,0}, tend={0,0};
 int i=0;
 
-while(i<1){
-i++
+clock_gettime(CLOCK_MONOTONIC, &tstart);
+while(pthread_spin_trylock()){
 }
+clock_gettime(CLOCK_MONOTONIC, &tend);
+
+printf("time: %5f seconds\n",((double)tend.tv_sec+1.0e-9*tend.tv_nsec)-((double)tstart.tv_sec+1.0e-9*tstart.tv_nsec));
+
+
 
 }
 
